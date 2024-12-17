5afcc5f5a71752795dc77a8829179082fa1307ed
vin1112 <vkvijethkumar24@gmail.com>
2024-12-17 12:26:10 +0530

    update config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+const API_BASE_URL = process.env.VITE_API_BASE_URL || "http://localhost:5000/";
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000/",
+        target: API_BASE_URL,
         changeOrigin: true,
       },
     },
