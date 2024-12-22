6f04e30fcfa9862162de32dd82d33a90a395c6c8
Vijeth Kumar <71746520+Vijethkumar24@users.noreply.github.com>
2024-12-22 11:25:27 +0530

    Update LoadingSpinner.jsx
    upadted Loading Spinner

--- frontend/src/components/common/LoadingSpinner.jsx
+++ frontend/src/components/common/LoadingSpinner.jsx
@@ -1,31 +1,6 @@
 const LoadingSpinner = ({ size = "md" }) => {
-  // Map size prop to Tailwind classes
-  const sizeMap = {
-    sm: "w-8 h-8 text-2xl",
-    md: "w-16 h-16 text-4xl",
-    lg: "w-24 h-24 text-6xl",
-  };
+  const sizeClass = `loading-${size}`;
 
-  // Get size class from the map
-  const sizeClass = sizeMap[size] || sizeMap.md; // Default to "md" if size not found
-
-  return (
-    <div data-theme="forest" className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="relative">
-        {/* Spinner */}
-        <div data-theme="forest"
-          className={`animate-spin border-t-4 border-blue-500 border-solid rounded-full ${sizeClass}`}
-        ></div>
-
-        {/* "X" Symbol */}
-        <div
-          className={`absolute inset-0 flex items-center justify-center text-blue-500 font-bold animate-pulse`}
-        >
-          X
-        </div>
-      </div>
-    </div>
-  );
+  return <span className={`loading loading-spinner ${sizeClass}`} />;
 };
-
 export default LoadingSpinner;
