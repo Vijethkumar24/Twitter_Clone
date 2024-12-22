dd19e6adcfddde7d704488ef960f614ea93b7daf
Vijeth Kumar <71746520+Vijethkumar24@users.noreply.github.com>
2024-12-22 11:24:45 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,6 @@ import NotificationPage from "./pages/notification/Notification.jsx";
 import ProfilePage from "./pages/profile/ProfilePage.jsx";
 import { Toaster } from "react-hot-toast";
 import { useQuery } from "@tanstack/react-query";
-import LoadingSpinner from "./components/common/LoadingSpinner.jsx";
 import axios from "axios";
 
 function App() {
@@ -40,8 +39,23 @@ function App() {
     }, // Fetch only if `jwt` exists in cookies
   });
   if (isLoading) {
-    return (
- <LoadingSpinner size="lg"/>
+    return (<>
+  <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="relative">
+          {/* Spinner */}
+          <div
+            className={`animate-spin border-t-4 border-blue-500 border-solid rounded-full ${sizeClass}`}
+          ></div>
+
+          {/* "X" Symbol */}
+          <div
+            className={`absolute inset-0 flex items-center justify-center text-blue-500 font-bold animate-pulse`}
+          >
+            X
+          </div>
+        </div>
+      </div>
+      </>
     );
   }
   return (
