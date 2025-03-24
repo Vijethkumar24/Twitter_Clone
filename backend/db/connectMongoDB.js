fda297614644113dd4d5d5b5b6ebe1108d81454a
Vijeth Kumar <71746520+Vijethkumar24@users.noreply.github.com>
2025-03-24 17:59:52 +0530

    Update connectMongoDB.js
    added ping to make db always awake

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -4,6 +4,14 @@ const connectMongoDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MONGODB connected${conn.connection.host}`);
+     setInterval(async () => {
+      try {
+        await mongoose.connection.db.command({ ping: 1 });
+        console.log("✅ MongoDB Pinged to Prevent Sleep");
+      } catch (error) {
+        console.error("⚠️ MongoDB Ping Error:", error);
+      }
+    }, 10 * 60 * 1000);
   } catch (error) {
     console.log(`Error:Connection Failure:${error}`);
     process.exit(0);
